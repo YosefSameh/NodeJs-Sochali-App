8f27e06c1b192919ee7c94a27972b98702da67a8
YosefSameh <139604190+YosefSameh@users.noreply.github.com>
2024-08-13 07:09:34 +0300

    Update users.modules.js

--- Modules/users.modules.js
+++ Modules/users.modules.js
@@ -1,83 +1,87 @@
-const Posts = require("../Modules/social.modules");
-const Users = require("../Modules/users.modules");
+const mongoose = require("mongoose")
+const validator = require("validatorjs")
 
-
-
-const SavePost = async (req,res)=>{
-
-    const postId = req.params.idPosts
-    const userId = req.user.id
-
-    
-        try {
-            const user = await Users.findById(userId);
-            if (!user) {
-                return res.status(404).json({ status: "Fail", data: { Message: "User not found" } });
-            }
-    
-            if (user.saved.includes(postId)) {
-                return res.status(400).json({ status: "Fail", data: { Message: "Post already saved" } });
-            }
-            
-            const post = await Posts.findById(postId)
-
-            console.log(post,"Posts");
-            
-            const newPostSave = {
-                postId:post._id,
-                titel:post.titel,
-                img:post.imgPost,
-                imgProfile:post.imgProfile,
-                createAt:new Date(),
-                firstName:post.firstName,
-                lastName:post.lastName,
-            }
-            
-            
-            
-            user.saved.push(newPostSave);
-            await user.save();
-            
-            res.json({ status: "Successful", data: { saved: user.saved} });
-        }
-        catch (error) {
-            res.status(500).json({ status: "Error", data: { Message: error.message } });
-        }
-
-}
-
-
-const UnSave = async (req,res)=>{
-
-    const userId = req.user.id
-    const saveId = req.params.idSave
-
-
-    try{
-        const user = await Users.findById(userId)
-    
-    const Unsave =  user.saved.findIndex(save => save._id.toString() === saveId)
-
-    // if(Unsave ===  0){
-    //     return res.status(404).json({ status: "fail", data: { message: "Save not found" } });
-    // }
-
-    console.log(Unsave);
-    
-    
-    user.saved.splice(Unsave,1)
-    await user.save()
-    res.json({ status: "successful", data: {user} });
-
-    }catch(error){
-        return res.status(404).json({ status: "fail", data: { message: error } });
+const FollowingSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    postId:{
+        type:String,
     }
+})
 
+const SavedSchema = new mongoose.Schema({
+    titel:{
+        type:String,
+        required:true
+    },
+    img:{
+        type:String,
+        
+    },
+    imgProfile:{
+        type:String,
+    },
+    firstName:{
+        type:String,
+    },
+    lastName:{
+        type:String,
+    },
+    comments:{
+        type:String,
+        
+    },
+    createAt:{
+        required:true,
+        type:Date,
+        default:Date.now
+    },
+    postId:{
+        type:String, 
+    }
     
-}
+})
+
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate: {
+            validator: function(v) {
+                const validation = new validator({ email: v }, { email: 'required|email' });
+                return validation.passes();
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    token:{
+        type:String,
+    },
+    imgProfile:{
+        type:String,
+    },
+    saved:[SavedSchema],
+    follow:[FollowingSchema],
+})
 
 
-module.exports = {
-    SavePost,
-    UnSave
-}
+module.exports = mongoose.model("User",userSchema)
